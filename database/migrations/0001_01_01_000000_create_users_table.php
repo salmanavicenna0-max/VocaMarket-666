0916d156a25966049d231e40267dc8b442228a92
fynnaly <ghifarinurrazaq@gmail.com>
2026-08-13 15:07:27 +0700

    adding atribut tabel user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('nis')->unique();
+            $table->char('nis', '12')->unique();
+            $table->string('email')->unique();
+            $table->string('email_verification');
             $table->string('password');
+            $table->enum('role', ['admin', 'siswa', 'pembeli', ]);
+            $table->boolean('verification_seller');
             $table->rememberToken();
             $table->timestamps();
         });
