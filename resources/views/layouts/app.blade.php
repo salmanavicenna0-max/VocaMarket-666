b67669691f8be2ba06bce993b45205d8610a72e1
pplg-02 <pplg-02@pplg-02s-MacBook-Air.local>
2026-08-13 23:51:41 -0700

    style: turunkan posisi garis pita dekorasi agar tidak menutupi logo yang sudah diperbesar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,10 +84,10 @@
         <div class="absolute top-0 left-0 w-full h-36 z-0 pointer-events-none overflow-hidden">
             <svg class="w-full h-full opacity-90" preserveAspectRatio="none" viewBox="0 0 1000 144">
                 <!-- Top Line (Blue) -->
-                <!-- Slanting earlier (260 to 320) to stay clear of the Search Bar -->
-                <path d="M -10 112 L 260 112 L 320 55 L 1010 55" fill="none" stroke="#0a84d4" stroke-width="4" stroke-linejoin="round" />
+                <!-- Slanting earlier (280 to 340) to stay clear of the Search Bar -->
+                <path d="M -10 134 L 280 134 L 340 55 L 1010 55" fill="none" stroke="#0a84d4" stroke-width="4" stroke-linejoin="round" />
                 <!-- Bottom Line (Yellow) -->
-                <path d="M -10 120 L 260 120 L 320 63 L 1010 63" fill="none" stroke="#ffb900" stroke-width="4" stroke-linejoin="round" />
+                <path d="M -10 142 L 280 142 L 340 63 L 1010 63" fill="none" stroke="#ffb900" stroke-width="4" stroke-linejoin="round" />
             </svg>
         </div>
 
