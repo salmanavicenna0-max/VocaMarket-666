a93fa1439d947c5fa4afeb7e1fd89fb423cc2607
Salmonnnn <salmanavicenna0@gmail.com>
2026-08-16 17:56:09 +0700

    update ke main

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,6 @@ class UserSeeder extends Seeder
             [
                 'name' => 'Admin',
                 'nis' => null,
-                'email_verification' => 'verified',
                 'password' => bcrypt('admin123'),
                 'role' => 'admin',
                 'verification_seller' => true,
